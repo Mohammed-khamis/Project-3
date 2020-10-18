24db61f5a59fbe8f1084448723fca54829e8bc5b
Mohammed-khamis <moh.o.khamis@gmail.com>
2020-10-19 02:18:52 +0300

    khamis commit

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const registerRouter = require('./routes/registerRoutes');
 require("dotenv").config();
 
 const app = express();
 
 //Middlewares
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(registerRouter);
 
