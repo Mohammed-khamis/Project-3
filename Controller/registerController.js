289deca8506a3931c9b449aa404645372008dd4d
Mohammed-khamis <moh.o.khamis@gmail.com>
2020-10-19 06:39:08 +0300

    khamis commit

--- Controller/registerController.js
+++ Controller/registerController.js
@@ -1,25 +1,16 @@
 const bcrypt = require("bcrypt");
-const accounts = require("../Database/accounts");
-
 
 const hashPassword = (password) => {
   return bcrypt.hash(password, Number(process.env.SALT));
 };
 
 const allAccounts = (req, res) => {
-  res.json(accounts);
+  
 };
 
 const creatNewAccount = async (req, res) => {
-  const account = {
-    name: req.body.name,
-    email: req.body.email,
-    password: await hashPassword(req.body.password),
-    posts: [],
-    friends: [],
-  };
-  accounts.push(account);
-  res.json(account);
+  
+  
 };
 
 const updateAccount = (req, res) => {};
