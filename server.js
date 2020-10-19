7ca46d644d7cd255851cb9daf43a827b65db465c
Mohammed-khamis <moh.o.khamis@gmail.com>
2020-10-19 06:15:13 +0300

    khamis commit

--- server.js
+++ server.js
@@ -1,4 +1,27 @@
+const mongoose = require('mongoose');
 const app = require('./app');
+
+require("dotenv").config();
+
+const db = mongoose.connection;
+
+const options = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(process.env.DB_URL, options, () => {
+  console.log("DB READY TO USE");
+});
+
+// handle error / sucess
+db.on("error", (err) => console.log("ERR:", err.message));
+
+db.on("connected", () => console.log("MONGO CONNECTED"));
+
+db.on("disconnected", () => console.log("MONGO DISCONNECTED"));
+
 
 const port = 3000 || process.env.PORT;
 app.listen(port, () => {
